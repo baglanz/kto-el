38be31ce89df199f4d05c319a06a3399f03fd926
baglanz <baglan.zhaksylyk98@gmail.com>
2026-02-08 18:15:05 +0500

    feat(result): added the ability to download the result
    feat(result): added the ability to download the result

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [BillSplitController::class, 'index'])->name('bill.index');
 Route::post('/calculate', [BillSplitController::class, 'calculate'])->name('bill.calculate');
+Route::get('/download-pdf/{bill}', [BillSplitController::class, 'downloadPdf'])->name('bill.download-pdf');
